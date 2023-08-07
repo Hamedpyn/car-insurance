243ccbe33893f3f1470651fff494038ec4c4309c
Hamed <hamedpyn@gmail.com>
2023-08-07 05:22:29 +0430

    This code controls element visibility based on user interaction.

--- assets/src/js/index.js
+++ assets/src/js/index.js
@@ -9,3 +9,24 @@ let
     submitBtn = document.querySelector('#submitBtn'),
     buyBtn = document.querySelector('#BuyBtn'),
     spinner = document.querySelector('#spinner');
+
+// Adds a click event listener to the backIcon element
+backIcon.addEventListener('click', () => {
+    toggleVisibility(container, finalResult);
+});
+
+// Defines a submit function
+function submit() {
+    // Adds a click event listener to the submitBtn element
+    submitBtn.addEventListener('click', () => {
+        toggleVisibility(finalResult, container);
+    });
+}
+
+// Function to hide elements
+function toggleVisibility(remove, add) {
+    // Remove the 'hide' class from the remove element
+    remove.classList.remove('hide');
+    // Add the 'hide' class to the add element
+    add.classList.add('hide');
+}
