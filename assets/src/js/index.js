a6f08941258d8449299fd36dc9bce550504080b1
Hamed <hamedpyn@gmail.com>
2023-08-07 05:16:07 +0430

    selecting elements

---
+++ assets/src/js/index.js
@@ -0,0 +1,11 @@
+// Select DOM Elements
+
+let
+    cars = document.querySelector('#cars select'),
+    years = document.querySelector('#years select'),
+    backIcon = document.querySelector('#insurance-type i'),
+    finalResult = document.querySelector('#insurance-type'),
+    container = document.querySelector('#car-insurance'),
+    submitBtn = document.querySelector('#submitBtn'),
+    buyBtn = document.querySelector('#BuyBtn'),
+    spinner = document.querySelector('#spinner');
